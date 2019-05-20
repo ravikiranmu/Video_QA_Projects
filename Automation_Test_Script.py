d8e7701d1e4f2cb81395d8b90d99d67bbc3dd300
ravikiranmu <ravikiran.mu@gmail.com>
2019-05-20 09:10:03 +0530

    Added some more test cases into automation test script

--- Automation_Test_Script.py
+++ Automation_Test_Script.py
@@ -107,11 +107,11 @@ def testtotalStreamDuration():
     subtitleManifestPostCut = subtitleManifestDownload(subtitleManifestURLPostCut)
     mediaManifestPreCut = mediaManifestDownload(mediaManifestURLPreCut)
     subtitleManifestPreCut = subtitleManifestDownload(subtitleManifestURLPreCut)
+    pattern = re.compile(r"(INF)+")
     
     # Media Manifest total duration post cut
     newlist1 = []
     avDurationPostCut = 0.0
-    pattern = re.compile(r"(INF)+")
     for item in mediaManifestPostCut:
         if pattern.search(item) != None:
             newlist1.append(item)
@@ -121,7 +121,6 @@ def testtotalStreamDuration():
     # Media Manifest total duration pre cut
     newlist2 = []
     avDurationPreCut = 0.0
-    pattern = re.compile(r"(INF)+")
     for item in mediaManifestPreCut:
         if pattern.search(item) != None:
             newlist2.append(item)
@@ -135,7 +134,7 @@ def testtotalStreamDuration():
     print "AV Duration(in mins):Pre Cut HLS Content", round(avDurationPreCut/60,0)
     print "AV Duration(in mins):Post Cut HLS Content", round(avDurationPostCut/60,0)
    
-    if differenceMediaDuraiton <= 2.0:
+    if str(round(differenceMediaDuraiton,0)) >= str(2.0):
         print "\nPass. Difference between AV duration(in mins) of pre-cut content and post cut content is", round(differenceMediaDuraiton)
     else:
         print "\nFail. Difference between AV duration(in mins) of pre-cut content and post cut content is", round(differenceMediaDuraiton), "\nExpected difference = 2mins"
@@ -143,7 +142,6 @@ def testtotalStreamDuration():
     # Subtitle Manifest total duration post cut
     newlist3 = []
     subtitleDurationPostCut = 0.0
-    pattern = re.compile(r"(INF)+")
     for item in subtitleManifestPostCut:
         if pattern.search(item) != None:
             newlist3.append(item)
@@ -153,29 +151,146 @@ def testtotalStreamDuration():
     # Subtitle Manifest total duration pre cut
     newlist4 = []
     subtitleDurationPreCut = 0.0
-    pattern = re.compile(r"(INF)+")
     for item in subtitleManifestPreCut:
         if pattern.search(item) != None:
             newlist4.append(item)
     for item in newlist4:
         subtitleDurationPreCut += float((item.split(':',1)[1]).split(',',1)[0])
         
-    differenceSubtitleDuraiton = (subtitleDurationPostCut - subtitleDurationPreCut)/60
+    differenceSubtitleDuraiton = (subtitleDurationPreCut - subtitleDurationPostCut)/60
     
     print "\nSubtitle Total Duration Test Result"
     print "---------------------------"
     print "Subtitle Duration(in mins):Pre Cut HLS Content", round(subtitleDurationPreCut/60,0)
     print "Subtitle Duration(in mins):Post Cut HLS Content", round(subtitleDurationPostCut/60,0)
    
-    if differenceMediaDuraiton <= 2.0:
+    if str(round(differenceSubtitleDuraiton)) >= str(2.0):
         print "\nPass. Difference between Subtitle duration(in mins) of pre-cut content and post cut content is", round(differenceMediaDuraiton)
     else:
         print "\nFail. Difference between Subtitle duration(in mins) of pre-cut content and post cut content is", round(differenceMediaDuraiton), "\nExpected difference = 2mins"
+
+def testChopOutDuration():
+    mediaManifestPostCut = mediaManifestDownload(mediaManifestURLPostCut)
+    subtitleManifestPostCut = subtitleManifestDownload(subtitleManifestURLPostCut)
+    mediaManifestPreCut = mediaManifestDownload(mediaManifestURLPreCut)
+    subtitleManifestPreCut = subtitleManifestDownload(subtitleManifestURLPreCut)
+    pattern = re.compile(r"(INF)+")
+    
+    #Identify the cut happening at media manifest file
+    last1 = len(mediaManifestPostCut) - 1
+    result1 = [[]]
+    for i, v in enumerate(mediaManifestPostCut):
+        if v == "#EXT-X-DISCONTINUITY":
+            result1[-1].append("#EXT-X-DISCONTINUITY")
+            if i != last1:
+                result1.append([])
+        else:
+            result1[-1].append(v)
+    
+    beforeCutAV = []
+    beforeCutDurationAV = 0.0
+    for item in result1[0]:
+        if pattern.search(item) != None:
+            beforeCutAV.append(item)
+    for item in beforeCutAV:
+        beforeCutDurationAV += float((item.split(':',1)[1]).split(',',1)[0])
+
+    afterCutAV = []
+    afterCutDurationAV = 0.0
+    for item in result1[1]:
+        if pattern.search(item) != None:
+            afterCutAV.append(item)
+    for item in afterCutAV:
+        afterCutDurationAV += float((item.split(':',1)[1]).split(',',1)[0])
+
+    newlist1 = []
+    avDurationPostCut = 0.0
+    for item in mediaManifestPostCut:
+        if pattern.search(item) != None:
+            newlist1.append(item)
+    for item in newlist1:
+        avDurationPostCut += float((item.split(':',1)[1]).split(',',1)[0])
+            
+    # Media Manifest total duration pre cut
+    newlist2 = []
+    avDurationPreCut = 0.0
+    for item in mediaManifestPreCut:
+        if pattern.search(item) != None:
+            newlist2.append(item)
+    for item in newlist2:
+        avDurationPreCut += float((item.split(':',1)[1]).split(',',1)[0])
+            
+    differenceMediaDuraiton = (avDurationPreCut - avDurationPostCut)/60
+    
+    print "\nChop Duration test result for AV:"
+    print "----------------------------------------"
+    
+    if str(avDurationPreCut/60) == str((beforeCutDurationAV/60)+(afterCutDurationAV/60)+differenceMediaDuraiton):
+        print "AV cut happened from {0} mins and resumed at {1}".format((beforeCutDurationAV/60),((beforeCutDurationAV/60)+differenceMediaDuraiton))
+    
+    #Identify the cut happening at subtitle manifest file    
+    last2 = len(subtitleManifestPostCut) - 1
+    result2 = [[]]
+    for i, v in enumerate(subtitleManifestPostCut):
+        if v == "#EXT-X-DISCONTINUITY":
+            result2[-1].append("#EXT-X-DISCONTINUITY")
+            if i != last2:
+                result2.append([])
+        else:
+            result2[-1].append(v)
+    
+    beforeCutSubtitle = []
+    beforeCutDurationSubtitle = 0.0
+    for item in result2[0]:
+        if pattern.search(item) != None:
+            beforeCutSubtitle.append(item)
+    for item in beforeCutSubtitle:
+        beforeCutDurationSubtitle += float((item.split(':',1)[1]).split(',',1)[0])
+
+    afterCutSubtitle = []
+    afterCutDurationSubtitle = 0.0
+    for item in result2[1]:
+        if pattern.search(item) != None:
+            afterCutSubtitle.append(item)
+    for item in afterCutSubtitle:
+        afterCutDurationSubtitle += float((item.split(':',1)[1]).split(',',1)[0])
+
+    # Subtitle Manifest total duration post cut
+    newlist3 = []
+    subtitleDurationPostCut = 0.0
+    for item in subtitleManifestPostCut:
+        if pattern.search(item) != None:
+            newlist3.append(item)
+    for item in newlist3:
+        subtitleDurationPostCut += float((item.split(':',1)[1]).split(',',1)[0])
+        
+    # Subtitle Manifest total duration pre cut
+    newlist4 = []
+    subtitleDurationPreCut = 0.0
+    for item in subtitleManifestPreCut:
+        if pattern.search(item) != None:
+            newlist4.append(item)
+    for item in newlist4:
+        subtitleDurationPreCut += float((item.split(':',1)[1]).split(',',1)[0])
+        
+    differenceSubtitleDuraiton = (subtitleDurationPreCut - subtitleDurationPostCut)/60
+    
+    print "\nChop Duration test result for Subtitle:"
+    print "----------------------------------------"
+    
+    if str(subtitleDurationPreCut/60) == str((beforeCutDurationSubtitle/60)+(afterCutDurationSubtitle/60)+differenceSubtitleDuraiton):
+        print "Subtitle cut happened from {0} mins and resumed at {1}".format((beforeCutDurationSubtitle/60),((beforeCutDurationSubtitle/60)+differenceSubtitleDuraiton))
+
+    if ((int(beforeCutDurationSubtitle/60) == int(beforeCutDurationAV/60)) and int(((beforeCutDurationSubtitle/60)+differenceSubtitleDuraiton)) == int(((beforeCutDurationAV/60)+differenceMediaDuraiton))):
+        print "\nAV is synced with Subtitle"
+    else:
+        print "\n AV is not synced with Subtitle"
 
 def main():
     testDiscontinuityOnMediaManifest()
     testDiscontinuityOnSubtitleManifest()
     testtotalStreamDuration()
+    testChopOutDuration()
     os.remove("prog_index.m3u8")
     os.remove("subtitle.m3u8")
     
